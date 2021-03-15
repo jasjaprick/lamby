400020973162bc3b0ee664e1519ee7c0795ec606
jose lamas <jllt1992@gmail.com>
2021-03-15 19:31:51 +0000

    tasting position halft way

--- server/test/matchTesting.js
+++ server/test/matchTesting.js
@@ -5,11 +5,10 @@ const app = require('../index');
 const API = 'http://localhost:3001';
 const db = require('../model');
 
-const mocksUser = require('./mocksUser');
 const mocksMatch = require('./mocksMatch');
 const mocksPositions = require('./mocksPositions');
 
-describe('check MATCH`S table', () => {
+describe('check MATCH`S table', async () => {
   it('check if MATCH`s table is emty (get)', async () => {
     const res = await request(API).get('/match');
     expect(res.statusCode).equal(200);
@@ -35,21 +34,43 @@ describe('check MATCH`S table', () => {
   });
 
   it('Create a new Matches (post)', async () => {
-    await request(API)
+    let res = await request(API)
       .post('/match')
       .set('Content-Type', 'application/json')
       .send(mocksMatch.mockMatch2);
-    expect(201);
+    expect(res.status).equal(201);
     let test = await db.Match.findAll();
     expect(test[3].dataValues.venue).equal('Madrid');
     expect(test.length).eql(4);
   });
 
   it('Create a new Matches (post-error-handler)', async () => {
-    await request(API)
+    let res = await request(API)
       .post('/match')
       .set('Content-Type', 'application/json')
       .send(mocksMatch.mockMatch7);
-    expect(500);
+    expect(res.statusCode).equal(500);
+  });
+});
+
+describe('check POSITION API', async () => {
+  it('check if POSITION`S table is emty (get)', async () => {
+    const res = await request(API).get('/positions');
+    expect(res.statusCode).equal(200);
+    expect(res.text).equal('[]');
+  });
+
+  it('Create a new Matches (post)', async () => {
+    let res = await request(API)
+      .post('/positions')
+      .set('Content-Type', 'application/json')
+      .send(mocksPositions.mockPosition3);
+    expect(res.status).equal(500);
+  });
+
+  it('Getting all POSITION before having data in USER DB(get)', async () => {
+    const res = await request(API).get('/positions');
+    expect(res.statusCode).equal(200);
+    expect(res.text).equal('[]');
   });
 });
--- server/test/mocksPositions.js
+++ server/test/mocksPositions.js
@@ -8,7 +8,7 @@ module.exports = {
   mockPosition2: {
     matchId: 1,
     userId: 2,
-    position: 'cb',
+    position: 'GK',
     instruction: 'its crazyyyyy',
   },
   mockPosition3: {
@@ -21,55 +21,12 @@ module.exports = {
     matchId: 1,
     userId: 4,
     position: 'cb',
-    instruction:
-      'jasja come onnnnn, you must be a developer not a football player',
+    instruction: 'come onnnnn, do you know how to code?',
   },
   mockPosition5: {
     matchId: 1,
-    userId: 5,
-    position: 'cb',
-    instruction: 'jaimitooo the covid make you fat',
-  },
-  mockPosition6: {
-    matchId: 1,
-    userId: 6,
+    userId: [6, 'a,', 1],
     position: 'cb',
     instruction: 'make the goalllllll',
-  },
-  mockPosition7: {
-    matchId: 1,
-    userId: 7,
-    position: 'cb',
-    instruction: 'its crazyyyyy',
-  },
-  mockPosition8: {
-    matchId: 1,
-    userId: 8,
-    position: 'cb',
-    instruction: 'yani, good job...',
-  },
-  mockPosition9: {
-    matchId: 1,
-    userId: 9,
-    position: 'cb',
-    instruction: 'jorge give me 50 push ups',
-  },
-  mockPosition10: {
-    matchId: 1,
-    userId: 10,
-    position: 'cb',
-    instruction: 'you need glasses or what is going on here?',
-  },
-  mockPosition11: {
-    matchId: 1,
-    userId: 11,
-    position: 'cb',
-    instruction: 'salva, its not a box bag',
-  },
-  mockPosition12: {
-    matchId: 1,
-    userId: 1,
-    position: 'cb',
-    instruction: 'jose, go for climbing anf forget this sport',
   },
 };
